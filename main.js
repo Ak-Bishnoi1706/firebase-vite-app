432248ed5ea76003085073cd4545622a0fe90fe9
Anita Bishnoi <anitakaliyana01@gmail.com>
2025-12-01 12:45:31 +0530

    Admin Dashboard
    view order page + view customers + admin profile

---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+    const express = require('express');
+    const path = require('path');
+    const app = express();
+
+// Serve static files from the 'public' directory
+    app.use(express.static(path.join(__dirname, 'public')));
+        
